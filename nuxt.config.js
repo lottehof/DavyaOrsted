5dc38d7845dc79b90d3d73a5092e0cf0f685ba94
lottehof <s1112052@student.hsleiden.nl>
2021-06-03 12:38:43 +0200

    davy&orsted

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
     target: 'static', // default is 'server'
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Davy-Ørsted',
+    title: 'DavyOrsted',
     htmlAttrs: {
       lang: 'en'
     },
